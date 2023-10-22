721d9a1b4a3935bd8289f5dd8f96dce68398241f
dwoura <568722524@qq.com>
2023-10-22 11:30:07 +0800

    Add files via upload

--- blog-sql.sql
+++ blog-sql.sql
@@ -1,1 +1,111 @@
-`blog_user``blog_category``blog_category``blog_user``users`
+/*
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 8.0.34 : Database - dwoura
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`dwoura` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `dwoura`;
+
+/*Table structure for table `blog_category` */
+
+DROP TABLE IF EXISTS `blog_category`;
+
+CREATE TABLE `blog_category` (
+  `cid` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`cid`) USING BTREE,
+  UNIQUE KEY `idx_cid` (`cid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `blog_category` */
+
+insert  into `blog_category`(`cid`,`name`,`create_at`,`update_at`) values 
+(1,'Go','2022-02-14 23:55:28','2022-02-14 23:55:28'),
+(2,'Java','2022-02-17 17:16:07','2022-02-17 17:16:07'),
+(3,'Rust','2023-10-21 17:58:33','2023-10-21 17:58:36'),
+(4,'Solidity','2023-10-21 17:58:52','2023-10-21 17:58:54'),
+(5,'数据结构','2023-10-21 17:59:22','2023-10-21 17:59:23'),
+(6,'算法','2023-10-21 17:59:31','2023-10-21 17:59:33'),
+(7,'操作系统','2023-10-21 17:59:41','2023-10-21 17:59:42'),
+(8,'英语','2023-10-22 09:00:38','2023-10-22 09:00:42');
+
+/*Table structure for table `blog_post` */
+
+DROP TABLE IF EXISTS `blog_post`;
+
+CREATE TABLE `blog_post` (
+  `pid` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `markdown` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `category_id` int NOT NULL,
+  `user_id` bigint NOT NULL,
+  `view_count` int NOT NULL DEFAULT '0',
+  `type` int NOT NULL DEFAULT '0',
+  `slug` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `create_at` datetime NOT NULL,
+  `update_at` datetime NOT NULL,
+  PRIMARY KEY (`pid`) USING BTREE,
+  UNIQUE KEY `idx_pid` (`pid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `blog_post` */
+
+/*Table structure for table `blog_user` */
+
+DROP TABLE IF EXISTS `blog_user`;
+
+CREATE TABLE `blog_user` (
+  `uid` bigint NOT NULL,
+  `user_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `passwd` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `avatar` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `create_at` datetime NOT NULL,
+  `update_at` datetime NOT NULL,
+  PRIMARY KEY (`uid`) USING BTREE,
+  UNIQUE KEY `idx_user_name` (`user_name`) USING BTREE,
+  UNIQUE KEY `idx_uid` (`uid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `blog_user` */
+
+insert  into `blog_user`(`uid`,`user_name`,`passwd`,`avatar`,`create_at`,`update_at`) values 
+(1,'admin','79c884320fd5146eeb7b84339bce9e95','dwoura','2023-10-12 22:57:11','2023-10-12 22:57:14');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `openid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `vip` tinyint NOT NULL,
+  `sc` tinyint NOT NULL,
+  `lt` tinyint NOT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  `subscribe` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
